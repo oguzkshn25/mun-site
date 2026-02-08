16a5d389cfc6a7b6a5927b068a750132ecc74bbe
oguzkshn25 <oksahin2525@gmail.com>
2026-02-08 21:59:40 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -45,3 +45,12 @@ const navLinks = document.getElementById("nav-links");
 menuToggle.addEventListener("click", () => {
   navLinks.classList.toggle("active");
 });
+
+
+
+document.querySelectorAll("#nav-links a").forEach(link => {
+  link.addEventListener("click", () => {
+    navLinks.classList.remove("active");
+  });
+});
+
